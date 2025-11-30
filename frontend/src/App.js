9fc95d78fd9a200a09b31c52b45c853e0194ed38
Naga Sriharsha Mulugu <sriharsha1103@gmail.com>
2025-11-30 11:20:38 +0530

    fix: remove BASE_URL from environment configuration and set static basename for BrowserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,7 +28,7 @@ function App() {
     <div className="App">
       <Notifications position="top-right" zIndex={1000}/>
       
-      <BrowserRouter basename={process.env.BASE_URL || '/'}>
+      <BrowserRouter basename='refolio'>
         <Routes>
           <Route path="/" element={<Main/>}/>
           <Route path="/home" element={<Home/>}/>
